8d9fbeff1fb562f2d527feebb8cc9322d60b42c3
ZhangHanming <zhang.hanming.official@gmail.com>
2016-09-10 17:56:21 +0800

    add association to user

--- models/user.js
+++ models/user.js
@@ -14,7 +14,11 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+          User.belongsTo(models.School, {
+              onUpdate: "CASCADE",
+              foreignKey: "schoolId"
+          });
+          User.hasMany(models.Deadline);
       }
     }
   });
