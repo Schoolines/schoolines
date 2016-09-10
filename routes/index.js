21da681a37a884b3526827b62c4e87359d6fcd55
kenoung <E0003273@u.nus.edu>
2016-09-11 04:26:52 +0800

    put on same page

--- routes/index.js
+++ routes/index.js
@@ -3,42 +3,78 @@ var https = require('https');
 var request = require('request');
 var router = express.Router();
 
-// IVLE API Key : Should store in config file
-const ivle_api_key = "UY5RaT4yK3lgWflM47CJo";
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render("layout");
 });
 
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-	var fullUrl = req.protocol + '://' + req.get('host') + '/home';
-	res.render("login", {api_key: ivle_api_key, path: fullUrl});
-});
+// START IVLE //
+const querystring = require('querystring');
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-	var token = req.query.token
-	getUserInfo(token, req);
-	res.render("home");
-});
+// IVLE API Key : Should store in config file
+const ivle_api_key = "UY5RaT4yK3lgWflM47CJo";
+
+/* Login Function */
+function loginUrl(req) {
+	var loginLink = "https://ivle.nus.edu.sg/api/login/?";
+	var urlPath = req.protocol + '://' + req.get('host') + req.originalUrl;
+	var args = querystring.stringify({apikey: ivle_api_key, url: urlPath});
+	return loginLink + args;
+}
 
 /* Function to get user data */
 function getUserInfo(token, req) {
-	request('https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?APIKey=' +
-		ivle_api_key + '&AuthToken=' + token, 
-		function(error, response, body) {
+	var profileLink = "https://ivle.nus.edu.sg/api/Lapi.svc/Profile_View?";
+	var args = querystring.stringify({APIKey: ivle_api_key, AuthToken: token});
+	request(profileLink + args,	function(error, response, body) {
 			if (!error && response.statusCode == 200) {
 				var json_data = JSON.parse(body).Results[0];
 				json_data.AuthToken = token;
-				json_data.School = 'National University of Singapore'; // 
-				request.post(req.protocol + '://' + req.get('host') + '/api/createUser').form(json_data);
+
+				// Temporary cos we no school data
+				json_data.School = 'National University of Singapore'; 
+
+				// Can send directly to create new user
+				// request.post(req.protocol + '://' + req.get('host') + '/api/createUser').form(json_data);
 				console.log(json_data);
 			} else {
 				throw error;
 			}
 	 	});
 };
+
+/* Function to get modules */
+function getUserModules(token) {
+	var profileLink = "https://ivle.nus.edu.sg/api/Lapi.svc/Modules?";
+	var args = querystring.stringify({APIKey: ivle_api_key, AuthToken: token});
+	request(profileLink + args,	function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var json_data = JSON.parse(body).Results;
+
+				// Can send directly to create new user
+				// request.post(req.protocol + '://' + req.get('host') + '/api/createUser').form(json_data);
+				var module_codes = [];
+				for (i = 0; i < json_data.length; i++) {
+					module_codes.push(json_data[i].CourseCode);
+				}
+				
+				console.log(module_codes);
+			} else {
+				throw error;
+			}
+	 	});	
+}
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+	var fullUrl = loginUrl(req);
+	var token = req.query.token;
+
+	// getUserInfo(token, req);
+	getUserModules(token);
+
+	res.render("login", {path: fullUrl});
+});
+// END IVLE //
 
 module.exports = router;
