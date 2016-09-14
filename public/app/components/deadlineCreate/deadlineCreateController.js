0c27f1f09c9843aaf404fe853d040fdf3de5f133
ZhangHanming <zhang.hanming.official@gmail.com>
2016-09-14 19:38:45 +0800

    change module option to dropdown when create deadline

--- public/app/components/deadlineCreate/deadlineCreateController.js
+++ public/app/components/deadlineCreate/deadlineCreateController.js
@@ -1,13 +1,16 @@
 'use strict'
 
-angular.module("schoolines").controller("deadlineCreateController", ["$route", "$scope", "$location", "AuthService", "DeadlineService", "Session",
-    function($route, $scope, $location, AuthService, DeadlineService, Session) {
+angular.module("schoolines").controller("deadlineCreateController", ["$route", "$scope", "$location", "AuthService", "DeadlineService", "Session", "IVLEService",
+    function($route, $scope, $location, AuthService, DeadlineService, Session, IVLEService) {
         AuthService.autologin().then(function(){
             $scope.deadline = {};
             $scope.deadline.userId = Session.userId;
         });
 
-
+        IVLEService.getModules(Session.token).then(function(){
+            $scope.modules= Session.modules;
+            console.log($scope.modules);
+        });
         $scope.createDeadline = function(){
             DeadlineService.create($scope.deadline).then(function(res){
                 $route.reload();
