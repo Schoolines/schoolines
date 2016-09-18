263a0d95dec5f41d567c23b58413c2d8bbf6b923
kenoung <E0003273@u.nus.edu>
2016-09-18 10:06:13 +0800

    fixed comma missing

--- public/app/components/deadlineDetail/deadlineDetailController.js
+++ public/app/components/deadlineDetail/deadlineDetailController.js
@@ -1,7 +1,7 @@
 'use strict'
 
 angular.module("schoolines").controller("deadlineDetailController",
-["$scope", "$location", "DeadlineService" "$window",
+["$scope", "$location", "DeadlineService", "$window",
 function($scope, $location, DeadlineService, $window){
     $scope.deadline = DeadlineService.deadlineDetail;
     $window.ga('send', 'pageview', { page: $location.url() });
