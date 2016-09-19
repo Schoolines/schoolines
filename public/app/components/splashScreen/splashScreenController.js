5845aa009d2774af9bcef684e394cfe9d803726f
Ten Zhi Yang <tzyinc@hotmail.com>
2016-09-20 04:17:49 +0800

    Added splash screen

--- public/app/components/splashScreen/splashScreenController.js
+++ public/app/components/splashScreen/splashScreenController.js
@@ -1,6 +1,7 @@
 'use strict'
 
-angular.module("schoolines").controller("splashScreenController", ['$location', function($location){
+angular.module("schoolines").controller("splashScreenController", ['$location', '$scope', function($location,$scope){
+	$scope.images = ["/images/bgbanner-long.png","/images/bgbanner-port.png","/images/nus.jpg","/images/homework.jpg"];
     $scope.login = function(){
         $location.path('/');
     }
