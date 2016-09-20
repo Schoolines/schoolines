fa28321761e438858f50a018986a5b256b181d4d
kenoung <E0003273@u.nus.edu>
2016-09-20 08:32:53 +0800

    app icon

--- public/images/app/components/splashScreen/splashScreenController.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-angular.module("schoolines").controller("splashScreenController", ['$location', function($location){
-    $scope.login = function(){
-        $location.path('/');
-    }
-}])
